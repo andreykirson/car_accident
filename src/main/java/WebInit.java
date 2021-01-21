8d031c5095f89028ee5a9e4ccc913ceaa58397c9
fruit <a.s.kirson@gmail.com>
2021-01-21 23:02:53 +1000

    0. Spring MVC [#364879]

--- src/main/java/WebInit.java
+++ src/main/java/WebInit.java
@@ -1,2 +1,18 @@
+import config.WebConfig;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+
 public class WebInit {
+    public void onStartup(ServletContext servletCxt) {
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(WebConfig.class);
+        ac.refresh();
+        DispatcherServlet servlet = new DispatcherServlet(ac);
+        ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
 }
---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("config")
+public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("./WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
