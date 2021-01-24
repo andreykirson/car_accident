f0f028618760218c880336c07011e1b853421be4
fruit <a.s.kirson@gmail.com>
2021-01-24 13:43:32 +1000

    0. Spring DataSource [#364897]

--- src/main/java/control/IndexControl.java
+++ src/main/java/control/IndexControl.java
@@ -3,8 +3,7 @@ package control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import repository.AccidentMem;
-import service.AccidentService;
+import repository.AccidentJdbcTemplate;
 
 /**
  * @author
@@ -16,17 +15,15 @@ import service.AccidentService;
 @Controller
 public class IndexControl {
 
-//    private final AccidentService accidentService;
-//
-//    public IndexControl(AccidentService accidentService) {
-//        this.accidentService = accidentService;
-//    }
-//
-//
-//    @GetMapping("/")
-//    public String index(Model model) {
-//        AccidentService accidentService = new AccidentService(new AccidentMem());
-//        model.addAttribute("accidents", accidentService.getAll());
-//        return "index";
-//    }
+    private final AccidentJdbcTemplate accidents;
+
+    public IndexControl(AccidentJdbcTemplate accidents) {
+        this.accidents = accidents;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("accidents", accidents.getAll());
+        return "index";
+    }
 }
